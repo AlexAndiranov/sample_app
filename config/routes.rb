4ddef6cfc2221a6b5cf8150ed746d655409ebfeb
Alex <alexandrianovmail@gmail.com>
2017-01-09 21:52:45 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 App3::Application.routes.draw do
   resources :users
-  root 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+  root  'static_pages#home'
+  #signin invalid root?
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/help',    to: 'static_pages#help',  via: 'get' 
+  match '/about',   to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   get '/static_pages/home', to: 'static_pages#home'
   get '/static_pages/help', to: 'static_pages#help'
   get '/static_pages/about', to: 'static_pages#about'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,9 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
-
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -96,6 +98,11 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_falsey }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    it(:remember_token) { should_not be_blank }
   end
 
 end
